ce1ce1c4033ebde829b3c1c0c787e37112253920
hehe <774525000@qq.com>
2021-10-15 18:22:32 +0800

    斗鱼

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ from danmu_helper import DanMuHelper
 
 if __name__ == '__main__':
     helper = DanMuHelper('wss://danmuproxy.douyu.com:8501/')
-    helper.start('1126960')
+    helper.start('288016')
